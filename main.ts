2095ee2f7269dbcd3965f00102732d633cd5a5dd
ibuki2003 <main@fuwa.dev>
2025-05-21 11:21:24 +0900

    feat: async lock

--- main.ts
+++ main.ts
@@ -1,19 +1,22 @@
 import { Secret } from "./secret.ts";
 import { AiWithMemory } from "./llm.ts";
 import { bot } from "./discord.ts";
+import { AsyncValue } from "@core/asyncutil/async-value";
+import { Lock } from "@core/asyncutil/lock";
 
 const TARGET_GUILD_ID = Secret.TARGET_GUILD_ID;
 const TARGET_CHANNEL_ID = Secret.TARGET_CHANNEL_ID;
 
-const ChannelAI = new Map<string, AiWithMemory>();
+const ChannelAI = new Map<string, Lock<AsyncValue<AiWithMemory>>>();
 
-function getAi(guildId: string, channelId: string): AiWithMemory {
+function getAi(guildId: string, channelId: string): Lock<AsyncValue<AiWithMemory>> {
   if (ChannelAI.has(channelId)) {
     return ChannelAI.get(channelId)!;
   }
   const ai = new AiWithMemory(guildId, channelId);
-  ChannelAI.set(channelId, ai);
-  return ai;
+  const lock = new Lock(new AsyncValue(ai));
+  ChannelAI.set(channelId, lock);
+  return lock;
 }
 
 
@@ -29,9 +32,11 @@ bot.events.messageCreate = async (message) => {
   // show typing indicator
   const t = setTimeout(async () => {
     await bot.helpers.triggerTypingIndicator(message.channelId);
-  }, 1000);
+  }, 500);
 
-  const resp_content = await ai.getResponse(message);
+  const resp_content = await ai.lock(async (ai) => {
+    return await (await ai.get()).getResponse(message);
+  });
 
   clearTimeout(t);
 
